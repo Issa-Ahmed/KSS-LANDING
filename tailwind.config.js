44fad60108f48c6776f05f4072579879b30d0510
Issa Ahmed <issaahmed764@gmail.com>
2024-02-20 14:13:31 +0300

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: { raleway: "sans-serif" },
+      height: { 100: "34rem" },
+    },
+  },
+  plugins: [],
+};
